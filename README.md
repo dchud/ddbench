1eda9e0b5d6465174add4ec5079da8101d70db7b
Daniel Chudnov <dchud@umich.edu>
2016-05-13 18:18:43 -0400

    basic model is working with confire configuration of each of the four datasets

--- README.md
+++ README.md
@@ -10,3 +10,58 @@ The datasets included under data/ are graciously made available by
 Database Group Leipzig:
 
   http://dbs.uni-leipzig.de/en/research/projects/object_matching/fever/benchmark_datasets_for_entity_resolution
+
+## Current status
+
+This is a nascent app lacking much refinement.  So far the datasets 
+above have been prepped for ready use in benchmarking and stored in
+the `data/` directory.  The app itself is configured with a YAML file
+that details the contents of each dataset and their corresponding
+field structures and mapping details for use with dedupe.  This 
+configuration is based on [confire](https://github.com/bbengfort/confire)
+which is included as a dependency, and the details are in 
+`conf/ddbench.yaml`.
+
+Our intent is to wrap the basic `ddbench.py` script, which processes
+one dataset one time, with sufficient instrumentation to enable
+multiple runs, report generation, and calculations of accuracy and
+performance metrics.  This work is pending.
+
+`ddbench.py` uses the provided perfect match files to auto-label a
+training set drawn from a random sample during each run of dedupe.
+It defaults to exact precision, and a reliability option may be 
+given to introduce simulated operator error during training.
+
+
+## Basic usage
+
+ddbench assumes a Python 3.5+ environment.  Ideally using a Python
+virtual environment, first activate and prep that environment using pip:
+
+    % pyvenv ENV
+    % source ENV/bin/activate
+    (ENV) % pip install -r requirements.txt
+
+To initiate a single run of the ddbench script, specify one of the
+provided datasets using the `--dataset` option:
+
+    % ./ddbench.py --dataset=abt-buy
+    % ./ddbench.py --dataset=amazon-googleproducts
+    % ./ddbench.py --dataset=dblp-acm
+    % ./ddbench.py --dataset=dblp-scholar
+
+The script in its current primitive form will output a lot of debugging
+info that should reassure you that it at least is working.  We will
+improve upon this shortly.
+
+You may also specify `--count`, the number of pairs to auto-label,
+and `--reliability`, a number between 0.0-1.0 indicating the
+reliability of a simulated human trainer.  By default, the auto-label
+process will always choose the right decision about a training pair;
+if you set `--reliability=0.98`, for example, it will be correct only
+98% of the time.
+
+For example, to time the entire run using the Abt-Buy dataset with
+a 99% accurate trainer and 100 training pairs:
+
+    % time ./ddbench.py --count=100 --reliability=0.99 --dataset=abt-buy
---
+++ ddbench.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+
+import argparse
+from collections import defaultdict
+import csv
+import logging
+from random import random
+import re
+
+from unidecode import unidecode
+
+import dedupe
+
+from config import settings
+
+
+parser = argparse.ArgumentParser('ddbench - dedupe benchmarking tool')
+parser.add_argument('-c', '--count', dest='count',
+                    default=50, type=int,
+                    help='number of uncertain pairs to auto-label')
+parser.add_argument('-d', '--dataset', dest='dataset',
+                    help='name of dataset to benchmark against (see data/)')
+parser.add_argument('-r', '--reliability', dest='reliability',
+                    default=1.0, type=float,
+                    help='Reviewer "reliability" (0.0-1.0, default 1.0)')
+parser.add_argument('-v', '--verbose', dest='verbose', default=False,
+                    action='store_true')
+
+
+def pre_process(v):
+    """Clean up data on the way in, following the lead of the
+    record linkage example in dedupe-examples."""
+
+    v = unidecode(v)
+    v = re.sub('[\n/:]', ' ', v)
+    v = re.sub("[-',]", '', v)
+    v = re.sub(' +', ' ', v)
+    v = v.strip().strip('"').strip("'").strip().lower()
+    return v or None
+
+
+def corpus_generator(fname, encoding='utf-8'):
+    with open(fname, encoding=encoding) as corpus_input:
+        for row in corpus_input:
+            yield pre_process(row)
+
+
+def load_csv(fname, return_type=dict, encoding='utf-8', field_map={}):
+    d = return_type()
+    with open(fname, encoding=encoding) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for i, row in enumerate(reader):
+            clean_row = dict([(field_map.get(k, k), pre_process(v))
+                              for k, v in row.items()])
+            if clean_row.get('price', ''):
+                v_price = clean_row['price'].replace('$', '')
+                v_price = v_price.replace(' gbp', '')
+                try:
+                    clean_row['price'] = float(v_price)
+                except:
+                    logging.debug('v_price: %s' % v_price)
+            if isinstance(d, list):
+                d.append(clean_row)
+            else:
+                d[str(i)] = clean_row
+    return d
+
+
+def auto_label(deduper, m1, m2, reliability, count=100):
+    """
+    Given a deduper and match files, automatically label <count>
+    uncertain pairs according to the given reliability.
+    """
+
+    # FIXME: how many?
+    for i in range(count):
+        n_match, n_distinct = (len(deduper.training_pairs['match']),
+                               len(deduper.training_pairs['distinct']))
+        uncertain_pairs = deduper.uncertainPairs()
+        labels = {'distinct': [], 'match': []}
+
+        for record_pair in uncertain_pairs:
+            r0 = record_pair[0]
+            r1 = record_pair[1]
+            is_match = False
+            # TODO: which order are they in?
+            id1 = r0['id']
+            id2 = r1['id']
+            if id2 in m1[id1]:
+                is_match = True
+            elif id1 in m2[id2]:
+                is_match = True
+            if random() > reliability:
+                # lie about match; no effect if reliability == 1.0
+                is_match = 1 - is_match
+
+            logging.debug('match: ' + str(is_match))
+            if is_match:
+                labels['match'].append(record_pair)
+            else:
+                labels['distinct'].append(record_pair)
+            # TODO: what about uncertainty?  should we fake some?
+
+            logging.debug("{0} positive, {1} negative".format(
+                    n_match, n_distinct))
+
+        deduper.markPairs(labels)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    if args.verbose or settings.get('DEBUG'):
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    if args.reliability < 0.0 and args.reliability > 1.0:
+        parser.error('Reliability must be within [0.0, 1.0]')
+
+    datasets = settings.get('datasets', {})
+    try:
+        dataset = datasets[args.dataset]
+    except KeyError:
+        parser.error('Please specify a --dataset from those provided in data/')
+
+    # read in the datasets, assuming two for record linkage
+    input_files = dataset['input_files']
+    if len(input_files) != 2:
+        parser.error('--dataset has != 2 input files')
+
+    path = dataset['path']
+    f1 = dataset['input_files'][0]
+    data_file_1 = f1['data_file']
+    field_map_1 = f1.get('field_map', {})
+    encoding_1 = f1.get('encoding', 'utf-8')
+    input_file_1 = load_csv('%s/%s' % (path, data_file_1), encoding=encoding_1,
+                            field_map=field_map_1)
+
+    f2 = dataset['input_files'][1]
+    data_file_2 = f2['data_file']
+    field_map_2 = f2.get('field_map', {})
+    encoding_2 = f2.get('encoding', 'utf-8')
+    input_file_2 = load_csv('%s/%s' % (path, data_file_2), encoding=encoding_2,
+                            field_map=field_map_2)
+
+    # parse field definitions
+    fields = []
+    for field_spec in dataset['fields']:
+        field = {'field': field_spec['field'], 'type': field_spec['type']}
+        if 'has_missing' in field_spec.keys():
+            field['has missing'] = field_spec['has_missing']
+        if 'corpus_file' in field_spec.keys():
+            corpus_path = '%s/%s' % (path, field_spec['corpus_file'])
+            encoding = dataset.get('encoding', 'utf-8')
+            field['corpus'] = corpus_generator(corpus_path, encoding=encoding)
+        fields.append(field)
+
+    logging.debug('Sampling')
+    linker = dedupe.RecordLink(fields)
+    # FIXME: 15000 is hard-coded and arbitrary, copied from dedupe example
+    linker.sample(input_file_1, input_file_2, 15000)
+
+    logging.debug('Loading match file')
+    # read in the match file now so we can auto-label
+    matches = load_csv('%s/%s' % (path, dataset['match_file']), list)
+
+    match_count = 0
+    # create fast lookup for each set of matching identifiers from one
+    # to the other, e.g. "if id2 in m1[id1]"
+    match_set_1 = defaultdict(set)
+    match_set_2 = defaultdict(set)
+
+    match_id_1 = dataset['input_files'][0]['match_id']
+    match_id_2 = dataset['input_files'][1]['match_id']
+
+    for m in matches:
+        match_count += 1
+        match_set_1[m[match_id_1]].add(m[match_id_2])
+        match_set_2[m[match_id_2]].add(m[match_id_1])
+
+    logging.debug('Auto-labeling')
+    auto_label(linker, match_set_1, match_set_2, args.reliability,
+               count=args.count)
+
+    logging.debug('Training')
+    linker.train()
+
+    logging.debug('Matching')
+    # FIXME: third param "0" is hard-coded
+    linked_records = linker.match(input_file_1, input_file_2, 0)
+
+    logging.debug('Reviewing linked records')
+
+    linked_records_count = 0
+    correct_count = incorrect_count = 0
+    for cluster, score in linked_records:
+        linked_records_count += 1
+        logging.debug('cluster %s, score %s' % (cluster, score))
+        is_correct = False
+        id1, id2 = cluster
+        r1 = input_file_1[id1]
+        r2 = input_file_2[id2]
+        if r2['id'] in match_set_1[r1['id']]:
+            is_correct = True
+        if r1['id'] in match_set_2[r2['id']]:
+            is_correct = True
+        if is_correct:
+            correct_count += 1
+            logging.debug('CORRECT')
+        else:
+            incorrect_count += 1
+            logging.debug('INCORRECT')
+
+    logging.debug('match_count: %s' % match_count)
+    logging.debug('linked_records_count: %s' % linked_records_count)
+    logging.debug('correct_count: %s' % correct_count)
+    logging.debug('incorrect_count: %s' % incorrect_count)
